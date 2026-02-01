01053f7d193929b8800c52d48757d7804def3ce4
AcacieS <acaciesong@gmail.com>
2026-02-01 08:07:51 -0500

    put in the asset of lvl1 + crow

--- gamejam/Assets/Scripts/CrowFollowMouse.cs
+++ gamejam/Assets/Scripts/CrowFollowMouse.cs
@@ -1,5 +1,4 @@
-using UnityEngine;
-
+// using UnityEngine;
 // public class CrowFollowMouse : MonoBehaviour
 // {
 //     [SerializeField] private float speed = 4f;
@@ -27,6 +26,7 @@ using UnityEngine;
 //         rb.linearVelocity = dir * speed;
 //     }
 // }
+using UnityEngine;
 
 public class CrowFollowMouse : MonoBehaviour
 {
@@ -34,24 +34,28 @@ public class CrowFollowMouse : MonoBehaviour
 
     private MouseInputProvider mouse;
     private Rigidbody2D rb;
+    private Collider2D col;
 
     void Awake()
     {
         mouse = FindObjectOfType<MouseInputProvider>();
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     void FixedUpdate()
     {
         Vector2 toMouse = mouse.WorldPosition - rb.position;
 
-        if (toMouse.sqrMagnitude < 0.01f)
+        // Approximate collider "radius"
+        float stopDistance = col.bounds.extents.magnitude;
+
+        if (toMouse.magnitude <= stopDistance)
         {
             rb.linearVelocity = Vector2.zero;
             return;
         }
 
-        Vector2 dir = toMouse.normalized;
-        rb.linearVelocity = dir * speed;
+        rb.linearVelocity = toMouse.normalized * speed;
     }
 }
--- gamejam/Assets/Scripts/Villager/Villager.cs
+++ gamejam/Assets/Scripts/Villager/Villager.cs
@@ -1,143 +1,3 @@
-// using System.Collections;
-// using UnityEngine;
-
-// public class Villager : MonoBehaviour
-// {
-//     private Animator anim;
-//     private Rigidbody2D rb;
-//     [SerializeField] private float moveSpeed = 1f;
-//     [SerializeField] private GameObject limitGuard;
-//     private VillagerState currentState = VillagerState.Walking;
-//     private float directionFactor = 1f;
-
-//     void Start()
-//     {
-//         anim = GetComponent<Animator>();
-//         rb = GetComponent<Rigidbody2D>();
-//         StartCoroutine(RandomMovementCoroutine());
-
-//     }
-//     public void SetCurrentState(VillagerState state)
-//     {
-//         if(currentState == VillagerState.Stun) return;
-//         currentState = state;
-//     }
-//     public void SetCurrentStateBack()
-//     {
-//         currentState = VillagerState.Idle;
-//         StopAllCoroutines();
-//         StartCoroutine(RandomMovementCoroutine());
-//     }
-
-//     public IEnumerator RandomMovementCoroutine()
-//     {
-//         while (currentState!=VillagerState.Checking)
-//         {
-//             yield return new WaitForSeconds(Random.Range(2, 5));
-//             RandomMovement();
-//         }
-        
-//     }
-//     private int direction = 0;
-//     private bool hasLimit = true;
-//     public void RandomMovement()
-//     {
-//         if(currentState == VillagerState.Checking) return;
-//         int choice = Random.Range(0, 2);
-//         hasLimit = true;
-//         switch (choice)
-//         {
-//             case 0:
-//                 currentState = VillagerState.Idle;
-//                 break;
-//             case 1:
-//                 SetWalk();
-//                 break;
-//             default:
-//                 currentState = VillagerState.Idle;
-//                 break;
-//         }
-//     }
-
-//     public void SetWalk()
-//     {
-//         currentState = VillagerState.Walking;
-//         direction = Random.Range(0, 2); // 0 = left, 1 = right
-//     }
-//     void Update()
-//     {
-//         if(currentState == VillagerState.Stun)
-//         {
-//             Stun();
-//         }else if(currentState == VillagerState.Checking)
-//         {
-//             Check();
-            
-//         }else if(currentState == VillagerState.Walking)
-//         {
-//             Walk();
-//         }
-//         else if(currentState == VillagerState.Idle)
-//         {
-//             Idle();
-//         }
-//         else
-//         {
-            
-//         }
-//         AnimationVillager();
-//     }
-//     public void Stun()
-//     {
-//         rb.linearVelocity = new Vector2(0, rb.linearVelocityY);
-//         rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-//         anim.SetTrigger("Stun");
-//         StopAllCoroutines();
-//     }
-//     public void RandomChoice()
-//     {
-        
-//     }
-//     public void Check()
-//     {
-//         //Debug.Log("Checking...");
-//         //TODO: Anis coding part
-//     }
-    
-//     private void AnimationVillager()
-//     {
-//         float speed = rb.linearVelocity.magnitude;
-//         anim.SetFloat("speed", speed);
-        
-//         if (rb.linearVelocity.x > 0)
-//         {
-//             directionFactor = 1f;
-//         }
-//         else if(rb.linearVelocity.x < 0)
-//         {
-//             directionFactor = -1f;
-//         }
-//         transform.localScale = new Vector3(directionFactor, transform.localScale.y,transform.localScale.z);
-//     }
-//     public void Walk()
-//     {
-//         //Debug.Log("Walk...");
-//         rb.linearVelocity = new Vector2(directionFactor * moveSpeed, rb.linearVelocityY);
-//         if(transform.position.x < limitGuard.transform.position.x)
-//         {
-//             directionFactor = 1;
-//         }
-//     }
-//     public void Idle()
-//     {
-//         //Debug.Log("Idle...");
-//         rb.linearVelocity = new Vector2(0, rb.linearVelocityY);
-//     }
-//     public void OnCollisionEnter2D(Collision2D other)
-//     {
-//         directionFactor = direction == 0 ? 1 : 0; // Reverse direction
-//     }
-// }
 using System.Collections;
 using UnityEngine;
 
@@ -151,8 +11,7 @@ public class Villager : MonoBehaviour
     [Header("Movement Limits")]
     [SerializeField] private Transform leftLimit;
     [SerializeField] private Transform rightLimit;
-    private bool hasLimit = true;
-
+    
     private VillagerState currentState = VillagerState.Walking;
 
     // 1 = right, -1 = left
@@ -306,7 +165,7 @@ public class Villager : MonoBehaviour
             directionFactor = -1f;
 
         transform.localScale = new Vector3(
-            directionFactor,
+            directionFactor * Mathf.Abs(transform.localScale.x),
             transform.localScale.y,
             transform.localScale.z
         );
