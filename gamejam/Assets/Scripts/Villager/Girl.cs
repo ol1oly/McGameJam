c2b78f92b57eb2fb2e8e218d9cf7f4a510bb37fe
AcacieS <acaciesong@gmail.com>
2026-02-01 06:41:45 -0500

    girl animation

--- gamejam/Assets/Scripts/Villager/Girl.cs
+++ gamejam/Assets/Scripts/Villager/Girl.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Girl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void NextLevel_2()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Scenes/Level2");
     }
 }
