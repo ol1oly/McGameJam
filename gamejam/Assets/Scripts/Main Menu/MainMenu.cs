e47daa0745319212785c5eebc947581add74f946
AcacieS <acaciesong@gmail.com>
2026-02-01 10:43:58 -0500

    lvl1

--- gamejam/Assets/Scripts/Main Menu/MainMenu.cs
+++ gamejam/Assets/Scripts/Main Menu/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
     public void LoadGame()
     {
         // load game -- first scene (currently general scene)
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void Quit()
@@ -18,7 +18,7 @@ public class MainMenu : MonoBehaviour
     public void LoadTutorial()
     {
         // load tutorial scene 
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 
     public void ReturnMenu()
